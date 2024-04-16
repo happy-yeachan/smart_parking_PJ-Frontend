65ad29c2d196cdd9b4451eb857f4a1c5c70c399b
yeachan <dus001228@naver.com>
2024-04-16 20:18:24 +0900

    새로고침 기능 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,9 +3,27 @@ import 'package:smart_parking/models/parkingLot_model.dart';
 import 'package:smart_parking/services/api_service.dart';
 import 'package:smart_parking/widgets/parkingLot_widget.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
-  final Future<List<parkingLotModel>> parkingLots = ApiService.getParkingLots();
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<parkingLotModel>> _parkingLots;
+
+  @override
+  void initState() {
+    super.initState();
+    _parkingLots = ApiService.getParkingLots();
+  }
+
+  Future<void> _refreshParkingLots() async {
+    setState(() {
+      _parkingLots = ApiService.getParkingLots();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +35,12 @@ class HomeScreen extends StatelessWidget {
         shadowColor: Colors.black,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: _refreshParkingLots,
+            icon: const Icon(Icons.refresh_sharp),
+          )
+        ],
         title: const Text(
           "Parking Spot",
           style: TextStyle(
@@ -25,21 +49,19 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: FutureBuilder(
-        future: parkingLots,
+        future: _parkingLots,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                Expanded(child: makeList(snapshot))
-              ],
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return makeList(snapshot);
           }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
         },
       ),
     );
